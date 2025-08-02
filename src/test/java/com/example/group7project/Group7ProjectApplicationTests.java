aafc741d58170f665e8ca31ad855e03fb3abfe42
Kien <ktruong29@csu.fullerton.edu>
2025-08-02 13:11:49 -0400

    Initial commit

---
+++ src/test/java/com/example/group7project/Group7ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group7project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group7ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
