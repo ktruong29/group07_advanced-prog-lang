3061c3fca6a1c15f64e8cb4643e23d685a80b42d
Kien <ktruong29@csu.fullerton.edu>
2025-08-08 15:18:33 -0400

    Added exception handler for customer service

--- src/main/java/com/example/group7project/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/group7project/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.group7project.handler;
 
 import com.example.group7project.dto.AppResponseDTO;
 import com.example.group7project.dto.ErrorDTO;
+import com.example.group7project.exception.CustomerServiceBusinessException;
 import com.example.group7project.exception.UserServiceBusinessException;
 import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
@@ -26,6 +27,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserServiceBusinessException.class)
     public AppResponseDTO<?> handleUserServiceBusinessException(UserServiceBusinessException ex) {
+        AppResponseDTO<?> appResponseDTO = new AppResponseDTO<>();
+
+        List<ErrorDTO> errors = List.of(new ErrorDTO(ex.getMessage()));
+
+        appResponseDTO.setStatus(HttpStatus.BAD_REQUEST);
+        appResponseDTO.setErrors(errors);
+
+        return appResponseDTO;
+    }
+
+    @ExceptionHandler(CustomerServiceBusinessException.class)
+    public AppResponseDTO<?> handleCustomerServiceBusinessException(CustomerServiceBusinessException ex) {
         AppResponseDTO<?> appResponseDTO = new AppResponseDTO<>();
 
         List<ErrorDTO> errors = List.of(new ErrorDTO(ex.getMessage()));
